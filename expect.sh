0809ed6a71ef2009b3b84416fd31657e1aeddd9d
Florent Jacquet <florent.jacquet@free-electrons.com>
2017-05-16 12:01:41 +0200

    Improve expect script
    Signed-off-by: Florent Jacquet <florent.jacquet@free-electrons.com>

--- expect.sh
+++ expect.sh
@@ -5,9 +5,17 @@ set timeout 40
 
 spawn telnet localhost 4000
 
-expect "buildroot login:"
+expect {
+    eof {puts "Connection problem, exiting."; exit 1}
+    timeout {puts "System did not boot in time, exiting."; exit 1}
+    "buildroot login:"
+}
 send "root\r"
-expect "# "
+expect {
+    eof {puts "Connection problem, exiting."; exit 1}
+    timeout {puts "No shell, exiting."; exit 1}
+    "# "
+}
 send "poweroff\r"
 expect "System halted"
 
