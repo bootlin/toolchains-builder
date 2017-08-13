29720e8e59f09c13dd51412ccf1674e4cf660b98
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-08-13 23:18:07 +0200

    build.sh: fix m68k coldfire qemu test command line
    The qemu_m68k_mcf5208_defconfig configuration is using a rootfs as
    initramfs, built into the kernel. Therefore, the Qemu command line
    should not try to use rootfs.ext2 as a disk drive, as it doesn't
    exist.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- build.sh
+++ build.sh
@@ -185,9 +185,8 @@ function set_test_config {
         test_defconfig="qemu_m68k_mcf5208_defconfig"
         qemu_system_command="qemu-system-m68k
             -machine mcf5208evb
+            -cpu m5208
             -kernel ${test_dir}/images/vmlinux
-            -drive file=${test_dir}/images/rootfs.ext2,index=0,media=disk,format=raw
-            -append \"root=/dev/hda rw\"
             -nographic"
     # elif [[ "${arch_name}" == "nios2" ]]; then                          # nios2 (no support in 2.8, coming in 2.9)
     #     test_defconfig="qemu_nios2_10m50_defconfig"
