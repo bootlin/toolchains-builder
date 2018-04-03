58a7ccd3e0bcf2e1e5efaf34dd6d4f53a3af37ae
Matt Weber <matthew.weber@rockwellcollins.com>
2018-04-03 07:34:36 -0500

    build.sh: bump bootstrap versions
    bleeding edge: jessie -> stretch
    stable: squeeze -> jessie
    
    Tested a toolchain build using both options on a Debian "stretch" host.
    
    Signed-off-by: Matthew Weber <matthew.weber@rockwellcollins.com>

--- build.sh
+++ build.sh
@@ -282,9 +282,9 @@ function launch_build {
 
     if [ ! -e ${chroot_dir}/.strapped ]; then
         if grep "bleeding-edge" <<<"${name}"; then
-            debootstrap --variant=buildd jessie ${chroot_dir} http://ftp.us.debian.org/debian/ || return 1
+            debootstrap --variant=buildd stretch ${chroot_dir} http://ftp.us.debian.org/debian/ || return 1
         else
-            debootstrap --variant=buildd squeeze ${chroot_dir} http://archive.debian.org/debian/ || return 1
+            debootstrap --variant=buildd jessie ${chroot_dir} http://ftp.us.debian.org/debian/ || return 1
         fi
     fi
     touch ${chroot_dir}/.strapped
