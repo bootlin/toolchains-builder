8a6e2708ade17b11ca1f2c77048d5cecb3f4f00a
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2021-12-26 22:26:11 +0100

    update_gitlab-ci.sh: create one pipeline per architecture
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- update_gitlab-ci.sh
+++ update_gitlab-ci.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-#
+# -*- mode: Shell-script; sh-basic-offset: 4; -*-
+
+set -e
+set -o pipefail
 
 base_dir=$(pwd)
 br_path=${base_dir}/buildroot
 frag_dir=${base_dir}/frags
 
 git_current_branch=$(git symbolic-ref -q --short HEAD)
-common_config="./configs/common.config"
-git_build_branch="builds-$(date +%Y-%m-%d--%H-%M-%S)"
 
 debug=0
 opt_arch="*"
 opt_libc="*"
 opt_variant="*"
 opt_target="no_push"
-opt_brtree="2017.05-toolchains-1"
+opt_brtree=""
 opt_version=""
 
 function clean_up {
@@ -59,34 +60,6 @@ Usage: $0 -n version [-a arch] [-l libc] [-v variant] [-t target] [-dh]
 EOF
 }
 
-while getopts "a:l:v:t:b:n:dh" opt; do
-    case "$opt" in
-    d) debug=1
-        ;;
-    a) opt_arch=$OPTARG
-        ;;
-    l) opt_libc=$OPTARG
-        ;;
-    v) opt_variant=$OPTARG
-        ;;
-    b) opt_brtree=$OPTARG
-        ;;
-    t) opt_target=$OPTARG
-        ;;
-    n) opt_version=$OPTARG
-        ;;
-    *|h|\?)
-        show_help
-        exit 0
-        ;;
-    esac
-done
-
-if [ -z $opt_version ] ; then
-	echo "ERROR: -n option is mandatory"
-	exit 1
-fi
-
 function check_config {
     cp ${config_file} ${br_path}/.config
     make -C ${br_path} olddefconfig 2>&1 1>/dev/null
@@ -127,31 +100,19 @@ function check_config {
     return 1
 }
 
-# Get buildroot if it's not done to check the configurations
-if [ ! -d ${br_path} ] ; then
-       git clone https://github.com/bootlin/buildroot-toolchains.git ${br_path}
-fi
-
-cd ${br_path} || exit 1
-git fetch origin || exit 1
-git reset --hard ${opt_brtree} || exit 1
-cd ${base_dir} || exit 1
-
-git branch -D ${git_build_branch} 2> /dev/null
-git checkout -b ${git_build_branch} || exit 1
-
-mkdir ${frag_dir}
-
-function add_to_ci {
-    arch_name=$(basename ${arch} .config)
-    libc_name=$(basename ${libc} .config)
-    variant_name=$(basename ${variant} .config)
-    name="${arch_name}-${libc_name}-${variant_name}"
-    extras=""
-    optionals=""
-    config_file=${frag_dir}/${name}
+function gen_fragment {
+    local arch_name=$1
+    local libc_name=$2
+    local variant_name=$3
+    local name="${arch_name}-${libc_name}-${variant_name}"
+    local extras=""
+    local optionals=""
+    local config_file=${frag_dir}/${name}
     printf "| %20s | %7s | %14s |" ${arch_name} ${libc_name} ${variant_name}
-    cat ${arch} ${libc} ${variant} ${common_config} > ${config_file}
+    cat configs/arch/${arch}.config \
+	configs/libc/${libc}.config \
+	configs/version/${variant}.config \
+	configs/common.config > ${config_file}
     for extra in $(ls -1 ${base_dir}/configs/extra/); do
         extra_m=${extra%.config}
         if [[ $name = $extra_m ]]; then
@@ -177,45 +138,123 @@ function add_to_ci {
     fi
 }
 
-function add_special {
-	special_name=$(basename ${special} .config)
-	cp ${special} ${frag_dir}/
+function prepare_git_branch {
+    local branch="$1"
+    git branch -D ${branch} || true
+    git checkout -b ${branch}
+
+    mkdir -p ${frag_dir}
 }
 
-cat .gitlab-ci.yml - > .gitlab-ci.yml.tmp <<EOF
-
+function submit_git_branch {
+    local branch="$1"
+    cat .gitlab-ci.yml - > .gitlab-ci.yml.tmp <<EOF
 variables:
     TOOLCHAIN_BUILDER_TARGET: "${opt_target}"
     TOOLCHAIN_BUILDER_BRTREE: "${opt_brtree}"
     TOOLCHAIN_BUILDER_VERSION: "${opt_version}"
 
 EOF
-mv .gitlab-ci.yml.tmp .gitlab-ci.yml
+    mv .gitlab-ci.yml.tmp .gitlab-ci.yml
+    git add frags/
+    git add -f .gitlab-ci.yml
+    git commit -m "Build bot: trigger new builds"
+    if [ "$opt_target" != "no_push" ]; then
+	git push -u -f git@gitlab.com:bootlin/toolchains-builder.git ${branch}
+    fi
+    git checkout $git_current_branch
+}
 
-if test "${opt_variant}" = "special" ; then
-	for special in $(ls ./configs/special/*.config); do
-		add_special
-	done
-else
-	printf "| %20s | %7s | %14s | %30s | %50s | status\n" "arch" "libc" "variant" "extras" "optionals"
-	echo
+function delete_git_branch {
+    local branch="$1"
+    git checkout $git_current_branch
+    git branch -D ${branch}
+}
 
-	for arch in $(ls ./configs/arch/${opt_arch}.config); do
-		for libc in $(ls ./configs/libc/${opt_libc}.config); do
-			for variant in $(ls ./configs/version/${opt_variant}.config); do
-				add_to_ci
-			done
-		done
+function handle_special {
+    local git_build_branch="builds-$(date +%Y-%m-%d--%H-%M-%S)"
+    prepare_git_branch ${git_build_branch}
+    for special in $(ls ./configs/special/*.config); do
+	special_name=$(basename ${special} .config)
+	cp ${special} ${frag_dir}/
+    done
+    submit_git_branch ${git_build_branch}
+}
+
+function handle_normal {
+    printf "| %20s | %7s | %14s | %30s | %50s | status\n" "arch" "libc" "variant" "extras" "optionals"
+    echo
+
+    for arch_config in $(ls ./configs/arch/${opt_arch}.config); do
+	local arch=$(basename ${arch_config} .config)
+	local git_build_branch="builds-${arch}-$(date +%Y-%m-%d--%H-%M-%S)"
+	prepare_git_branch ${git_build_branch}
+
+	for libc_config in $(ls ./configs/libc/${opt_libc}.config); do
+	    local libc=$(basename ${libc_config} .config)
+
+	    for variant_config in $(ls ./configs/version/${opt_variant}.config); do
+		local variant=$(basename ${variant_config} .config)
+		gen_fragment ${arch} ${libc} ${variant}
+	    done
 	done
+
+	ls frags/
+
+	nfrags=$(ls -1 ${frag_dir} | wc -l)
+	if test ${nfrags} -eq 0; then
+	    delete_git_branch ${git_build_branch}
+	else
+	    submit_git_branch ${git_build_branch}
+	fi
+    done
+}
+
+function main {
+    # Get buildroot if it's not done to check the configurations
+    if [ ! -d ${br_path} ] ; then
+	git clone https://github.com/bootlin/buildroot-toolchains.git ${br_path}
+    fi
+
+    # Grab the specified version of Buildroot
+    pushd ${br_path}
+    git fetch origin
+    git reset --hard ${opt_brtree}
+    popd
+
+    if test "${opt_variant}" = "special" ; then
+	handle_special
+    else
+	handle_normal
+    fi
+}
+
+while getopts "a:l:v:t:b:n:dh" opt; do
+    case "$opt" in
+    d) debug=1
+        ;;
+    a) opt_arch=$OPTARG
+        ;;
+    l) opt_libc=$OPTARG
+        ;;
+    v) opt_variant=$OPTARG
+        ;;
+    b) opt_brtree=$OPTARG
+        ;;
+    t) opt_target=$OPTARG
+        ;;
+    n) opt_version=$OPTARG
+        ;;
+    *|h|\?)
+        show_help
+        exit 0
+        ;;
+    esac
+done
+
+if [ -z $opt_version ] ; then
+	echo "ERROR: -n option is mandatory"
+	exit 1
 fi
 
-git add frags/
-git add -f .gitlab-ci.yml
-git commit -m "Build bot: trigger new builds"
-if [ "$opt_target" != "no_push" ]; then
-    git remote add gitlab git@gitlab.com:bootlin/toolchains-builder.git
-    git push -u -f gitlab ${git_build_branch}
-fi
-
-git checkout $git_current_branch
-
+main
